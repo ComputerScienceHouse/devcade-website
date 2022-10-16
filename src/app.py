fbf8fc8a434607ae4f020843e17a2dbe39790000
Andrew Simonson <asimonson1125@gmail.com>
2022-10-16 17:32:23 -0400

    create core pages, basic nav

--- src/app.py
+++ src/app.py
@@ -2,10 +2,29 @@ import flask
 from flask_login import login_required, current_user
 from auth import app
 
+guest = {
+    'name': 'Guest',
+    'username': 'Guest',
+    'img': './static/images/guest.png',
+    'admin': False
+}
+
 @app.route('/')
 # @login_required
 def homepage():
-    return flask.render_template('home.html')
+    return flask.render_template('home.html', user=guest)
+
+@app.route('/about')
+def aboutpage():
+    return flask.render_template('about.html', user=guest)
+
+@app.route('/catalog')
+def catalogpage():
+    return flask.render_template('catalog.html', user=guest)
+
+@app.route('/upload')
+def uploadpage():
+    return flask.render_template('upload.html', user=guest)
 
 if __name__ == '__main__':
     app.run()
