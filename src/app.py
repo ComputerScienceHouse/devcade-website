f002c9c3e393fe2128649af4e8a6d61ea2c05eb5
Andrew Simonson <asimonson1125@gmail.com>
2022-10-17 13:32:13 -0400

    sample user-only + socials

--- src/app.py
+++ src/app.py
@@ -2,29 +2,22 @@ import flask
 from flask_login import login_required, current_user
 from auth import app
 
-guest = {
-    'name': 'Guest',
-    'username': 'Guest',
-    'img': './static/images/guest.png',
-    'admin': False
-}
-
 @app.route('/')
 # @login_required
 def homepage():
-    return flask.render_template('home.html', user=guest)
+    return flask.render_template('home.html')
 
 @app.route('/about')
 def aboutpage():
-    return flask.render_template('about.html', user=guest)
+    return flask.render_template('about.html')
 
 @app.route('/catalog')
 def catalogpage():
-    return flask.render_template('catalog.html', user=guest)
+    return flask.render_template('catalog.html')
 
 @app.route('/upload')
 def uploadpage():
-    return flask.render_template('upload.html', user=guest)
+    return flask.render_template('upload.html', title='Devcade - Upload')
 
 if __name__ == '__main__':
     app.run(host='localhost')
