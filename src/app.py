228081821736ad8cd939341a905d0085f49242db
Wilson McDade <wilson.mcdade@gmail.com>
2023-03-29 01:48:46 -0400

    Added gamejam page. Using placeholder flyer for now

--- src/app.py
+++ src/app.py
@@ -95,6 +95,10 @@ def static_from_root():
 def credits():
     return flask.render_template('credits.html', contributors = contributors.contributors)
 
+@app.route('/gamejam')
+def gamejam():
+    return flask.render_template('gamejam.html')
+
 @app.errorhandler(Exception)
 def page404(e):
     eCode = 500
